e35805aeeefb97c9c6280a9e3ed5617c24698c20
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2025-01-13 21:40:37 +0100

    Examen criptograma - Incompleto

---
+++ Tema3_String/src/examen/Criptograma.java
@@ -0,0 +1,34 @@
+package examen;
+
+public class Criptograma {
+
+	// Creamos el array bidimensional abecedario como char para almacenar todas las
+	// letras del abecedario.
+	static char abecedario[][] = { { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' },
+			{ 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'k' }, { 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' } };
+
+	// Creamos el array frases como String para almacenar las frases con la que
+	// jugaremos y debemos de adivinar.
+	static String frases[] = {
+			"las guerras seguiran mientras el color de la piel siga siendo mas importante que el de los ojos",
+			"aprende a vivir y sabras morir bien", "cada dia sabemos mas y entendemos menos",
+			"el dinero no puede comprar la vida", "la verdadera sabiduria esta en reconocer la propia ignorancia" };
+
+	// Creamos la variable fraseReal como String para almacenar la frase con la que
+	// vamos a jugar.
+	static String fraseReal = "";
+
+	// Creamos la variable fraseCodificada como String para almcenar la codificación
+	// de la fraseReal.
+	String fraseCodificada = "";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	static public void desordenaAbecedario() {
+		
+	}
+
+}
