437ecfdcfd3cff33433133fc8e513ca81b6c8cda
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2025-01-14 12:16:09 +0100

    Examen Criptograma - Incompleto

--- Tema3_String/src/examen/Criptograma.java
+++ Tema3_String/src/examen/Criptograma.java
@@ -1,11 +1,14 @@
 package examen;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Criptograma {
 
 	// Creamos el array bidimensional abecedario como char para almacenar todas las
 	// letras del abecedario.
 	static char abecedario[][] = { { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' },
-			{ 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'k' }, { 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' } };
+			{ 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q' }, { 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' } };
 
 	// Creamos el array frases como String para almacenar las frases con la que
 	// jugaremos y debemos de adivinar.
@@ -23,12 +26,35 @@ public class Criptograma {
 	String fraseCodificada = "";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		desordenaAbecedario();
+		
+		System.out.println(Arrays.deepToString(abecedario));
 	}
 	
 	static public void desordenaAbecedario() {
 		
+		int posF;
+		
+		int posC;
+		
+		char caracter;
+		
+		Random rand = new Random();
+		
+		for (int i = 0; i < abecedario.length; i++) {
+			for (int j = 0; j < abecedario[i].length; j++) {
+				
+			caracter = abecedario[i][j];
+			posF = rand.nextInt(0, 3);
+			posC = rand.nextInt(0, 9);
+			
+			abecedario[i][j] = abecedario[posF][posC];
+			
+			abecedario[posF][posC] = caracter;
+			
+			}
+		}
 	}
 
 }
